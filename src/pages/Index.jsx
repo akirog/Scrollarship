4533ca2ec0c05b187d1fee643d245c4ecc1a10a5
kofra002 <kofra002@osloskolen.no>
2026-05-18 08:34:54 +0200

    Revert "moved get started button navigate event into its button onclick function instead of hiding a nav link in the button"
    This reverts commit 64588c2007c6cc996984628213b2cddf6da0a506.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -55,7 +55,9 @@ function Hero() {
       <div>
         <h1>Learn Better Faster</h1>
         <p>Scrollarship is a platform tailored for people wanting to improve their academic performance through engaging and interactive learning</p>
-        <button>Get Started</button>
+        <button className='heroButton'>
+          <NavLink to='/explore' state={{ signUp: true }}>Get Started</NavLink>
+        </button>
       </div>
       <div>
         <img src='/src/assets/scrolling.svg' />
